d1a6b434878d1096eb4acbcad914fb00753b08ba
therealjtgill <jtgill@email.arizona.edu>
2020-04-29 17:10:02 -0700

    Fixed bug that disallowed client.py from receiving messages from the server. Subscriptions were being registered before a connection was made to the broker.

--- script_lake/client.py
+++ script_lake/client.py
@@ -17,10 +17,10 @@ class Client(object):
       self.client.on_message = self.on_message
       self.client.on_connect = self.on_connect
 
+      self.client.connect(self.config.broker_url, self.config.broker_port)
+
       for topic in self.config.topics:
          self.client.subscribe(topic.name, qos=1)
-
-      self.client.connect(self.config.broker_url, self.config.broker_port)
 
       self.listen_topics = [
          e.name for e in self.config.topics if e.action == "listen"
